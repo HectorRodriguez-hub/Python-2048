80c64b1f8d5051d845758ef803fad9a843ba29ec
camilomoreno <camilomoreno@javeriana.edu.co>
2021-04-12 17:08:19 -0500

    Ahora si arriba + izq

--- Moreno_Ramirez_Rodriguez.py
+++ Moreno_Ramirez_Rodriguez.py
@@ -44,18 +44,12 @@ def start(M):
     M[0][1] = 2
     M[0][2] = 2
     M[0][3] = 2
-    M[1][0] = 2
-    M[1][1] = 2
+   
     M[1][2] = 2
-    M[1][3] = 2
+
     M[2][0] = 2
     M[2][1] = 2
-    M[2][2] = 2
-    M[2][3] = 2
-    M[3][0] = 2
-    M[3][1] = 2
-    M[3][2] = 2
-    M[3][3] = 2
+    
 
     return M
 #end def
@@ -65,37 +59,45 @@ def arriba(M):
         j = 0 
         k = j+1 
         while j < tam and k < tam:
-            if M[i][j] == M[i][k] and M[i][j] != 0:
-                M[i][j] = M[i][j]*2
-                M[i][k] = 0
-                j = j+1
-                k = j+1
-            else:
-                if M[i][j] == 0 and M[i][k] != 0:
-                    M[i][j] = M[i][k]
+            if M[i][j] == 0 and M[i][k] != 0:
+                M[i][j] = M[i][k]
+                M[i][k] = 0  
+            else: 
+                if M[i][j] == M[i][k] and M[i][j] != 0:
+                    M[i][j] = M[i][j]*2
                     M[i][k] = 0
-                    k = k+1
-                else:
-                    k = k+1
+                    j = j + 1
+                    k = j + 1
+                else: 
+                    if  M[i][j] !=0 and M[i][k] != 0 :
+                        j = j+1 
+                        k = j+1
+                    else: 
+                        if M[i][k] == 0: 
+                            k = k+1
     return M
 #end def
 def izquierda(M):
     for j in range(tam):
         i = 0 
         k = i+1 
-        while i < tam and k < tam:
-            if M[i][j] == M[k][j] and M[i][j] != 0:
-                M[i][j] = M[i][j]*2
-                M[k][j] = 0
-                i = i+1
-                k = k+1
-            else:
-                if M[i][j] == 0 and M[k][j] != 0:
-                    M[i][j] = M[k][j]
+        while j < tam and k < tam:
+            if M[i][j] == 0 and M[k][j] != 0:
+                M[i][j] = M[k][j]
+                M[k][j] = 0  
+            else: 
+                if M[i][j] == M[k][j] and M[i][j] != 0:
+                    M[i][j] = M[i][j]*2
                     M[k][j] = 0
-                    k = k+1
-                else:
-                    k = k+1
+                    i = i + 1
+                    k = i + 1
+                else: 
+                    if  M[i][j] !=0 and M[k][j] != 0 :
+                        i = i+1 
+                        k = i+1
+                    else: 
+                        if M[k][j] == 0: 
+                            k = k+1
     return M
 #end def
 
@@ -108,7 +110,10 @@ arriba(M)
 Print(M)
 izquierda(M)
 Print(M)
-
+izquierda(M)
+Print(M)
+arriba(M)
+Print(M)
 
 
 
