616009bee92cc9a917f2f02bf748f38b74e0c5c7
camilomoreno <camilomoreno@javeriana.edu.co>
2021-04-11 23:06:35 -0500

    Arriba + izquierda

--- Moreno_Ramirez_Rodriguez.py
+++ Moreno_Ramirez_Rodriguez.py
@@ -10,8 +10,8 @@ tam=4
 game=True
 #funciones de impresion
 def Print(M):
-    for i in range(len(M)):
-        for j in range(len(M[0])):
+    for j in range(len(M)):
+        for i in range(len(M[0])):
             
             if M[i][j] == math.inf:
                 print("X","",end = "")
@@ -40,28 +40,77 @@ def start(M):
     while i == a and j ==b:
         a = random.randint(0,3)
         b = random.randint(0,3)
-    M[3][0] = 2
+    M[0][0] = 2
+    M[0][1] = 2
+    M[0][2] = 2
+    M[0][3] = 2
     M[1][0] = 2
+    M[1][1] = 2
+    M[1][2] = 2
+    M[1][3] = 2
+    M[2][0] = 2
+    M[2][1] = 2
+    M[2][2] = 2
+    M[2][3] = 2
+    M[3][0] = 2
+    M[3][1] = 2
+    M[3][2] = 2
+    M[3][3] = 2
+
     return M
 #end def
 
-def prueba(M):
-    for j in range(tam):
-        for i in range(tam):
-            if i == 0 and j == 0:
-                M[j][i] = M[j][i]+ M[j][i]
-                M[j+1 ][i] = 0 
-    return M            
+def arriba(M):
+    for i in range(tam):
+        j = 0 
+        k = j+1 
+        while j < tam and k < tam:
+            if M[i][j] == M[i][k] and M[i][j] != 0:
+                M[i][j] = M[i][j]*2
+                M[i][k] = 0
+                j = j+1
+                k = j+1
+            else:
+                if M[i][j] == 0 and M[i][k] != 0:
+                    M[i][j] = M[i][k]
+                    M[i][k] = 0
+                    k = k+1
+                else:
+                    k = k+1
+    return M
 #end def
-
+def izquierda(M):
+    for j in range(tam):
+        i = 0 
+        k = i+1 
+        while i < tam and k < tam:
+            if M[i][j] == M[k][j] and M[i][j] != 0:
+                M[i][j] = M[i][j]*2
+                M[k][j] = 0
+                i = i+1
+                k = k+1
+            else:
+                if M[i][j] == 0 and M[k][j] != 0:
+                    M[i][j] = M[k][j]
+                    M[k][j] = 0
+                    k = k+1
+                else:
+                    k = k+1
+    return M
+#end def
 
 #LLamado a funciones
 M = crear_Matriz(tam)
 Print(M)
 start(M)
 Print(M)
-prueba(M)
+arriba(M)
 Print(M)
+izquierda(M)
+Print(M)
+
+
+
 
 
 """ while game:
